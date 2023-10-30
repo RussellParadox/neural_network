a85075c6908105fe1eccbf36406ad621a2539936
tacos_non_fumeur <geoffrey.d2002@gmail.com>
2023-10-30 16:38:43 +0100

    nn_init need to be tested

--- libnn/nn_init.c
+++ libnn/nn_init.c
@@ -15,29 +15,81 @@
 
 #include "nn.h"
 
-//layer_size[0]: size of input layer
-//layer_size[1]: size of hidden layer
-//layer_size[2]: size of output layer
-t_neural_network	*nn_new(unsigned int layer_quantity, unsigned int layer_size[3], float weight_min, float weight_max)
+static int	init_node(t_neural_network *nn, unsigned int layer_size[4])
+{
+	unsigned int	i;
+
+	nn->node[0] = matrix_new(layer_size[1], 1);
+	if (nn->node[0] == NULL)
+		return (-1);
+	i = 1;
+	while (i < layer_size[0] - 1)
+	{
+		nn->node[i] = matrix_new(layer_size[2], 1);
+		if (nn->node[i] == NULL)
+			return (-1);
+		i++;
+	}
+	nn->node[i] = matrix_new(layer_size[3], 1);
+	if (nn->node[i] == NULL)
+		return (-1);
+	nn->node[i + 1] = NULL;
+	return (0);
+}
+
+static int	init_weight(t_neural_network *nn, unsigned int layer_size[4], float weight_min, float weight_max)
+{
+	unsigned int	i;
+
+	nn->weight[0] = matrix_new(layer_size[2], layer_size[1]);
+	if (nn->weight[0] == NULL)
+		return (-1);
+	matrix_init(nn->weight[0], NULL, weight_min, weight_max);
+	i = 1;
+	while (i < layer_size[0] - 2)
+	{
+		nn->weight[i] = matrix_new(layer_size[2], layer_size[2]);
+		if (nn->weight[i] == NULL)
+			return (-1);
+		matrix_init(nn->weight[i], NULL, weight_min, weight_max);
+		i++;
+	}
+	nn->weight[i] = matrix_new(layer_size[3], layer_size[2]);
+	if (nn->weight[i] == NULL)
+		return (-1);
+	matrix_init(nn->weight[i], NULL, weight_min, weight_max);
+	nn->weight[i + 1] = NULL;
+	return (0);
+}
+
+//layer_size[0]: layer quantity
+//layer_size[1]: size of input layer
+//layer_size[2]: size of hidden layer
+//layer_size[3]: size of output layer
+t_neural_network	*nn_new(unsigned int layer_size[4], float weight_min, float weight_max)
 {
 	t_neural_network	*nn;
 
 	nn = (t_neural_network *)malloc(sizeof(t_neural_network));
 	if (nn == NULL)
 		return (NULL);
-	nn->node = (t_matrix **)malloc(sizeof(t_matrix *) * layer_quantity);
+	nn->node = (t_matrix **)malloc(sizeof(t_matrix *) * (layer_size[0] + 1));
 	if (nn->node == NULL)
 	{
 		nn_free(nn);
 		return (NULL);
 	}
-	nn->weight = (t_matrix **)malloc(sizeof(t_matrix *) * (layer_quantity - 1));
+	nn->weight = (t_matrix **)malloc(sizeof(t_matrix *) * layer_size[0]);
 	if (nn->weight == NULL)
 	{
-		free(nn->weight);
-		return (-1);
+		nn_free(nn);
+		return (NULL);
 	}
-	nn->node[0] = matrix_new(layer_size[1], 1);
-	if (nn->node[0] == NULL)
-
+	nn->node[0] = NULL;
+	nn->weight[0] = NULL;
+	if (init_node(nn, layer_size))
+		return (NULL);
+	if (init_weight(nn, weight_min, weight_max))
+		return (NULL);
+	return (nn);
 }
